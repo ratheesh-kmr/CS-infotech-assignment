cb4b1114374c674b6bb0cfa175250819996e7799
ratheesh-kmr <ratheeshkmr2024@gmail.com>
2025-03-18 15:24:36 +0530

    protectedRoutes have been installed and login based on JWT token

--- CS-task/frontend/react-app/src/App.jsx
+++ CS-task/frontend/react-app/src/App.jsx
@@ -2,15 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import AgentManagement from "./pages/agentManagement";
 import FileUpload from "./components/FileUpload";
+import ProtectedRoute from "./components/protectedRoutes";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        {/* <Route path="/agents" element={<ProtectedRoute><AgentManagement /></ProtectedRoute>} /> */}
-        <Route path="/agent" element = {<AgentManagement />} />
-        <Route path="/upload" element={<FileUpload />} />
+        <Route path="/agent" element={<ProtectedRoute><AgentManagement /></ProtectedRoute>} />
+        <Route path="/upload" element={<ProtectedRoute><FileUpload /></ProtectedRoute>} />
       </Routes>
     </Router>
   );
--- CS-task/frontend/react-app/src/components/protectedRoutes.jsx
+++ CS-task/frontend/react-app/src/components/protectedRoutes.jsx
@@ -1,8 +1,10 @@
+import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
-  return token ? children : <Navigate to="/" replace />;
+  const token = localStorage.getItem("token"); // Example: Check for JWT Token
+
+  return token ? children : <Navigate to="/" />;
 };
 
 export default ProtectedRoute;
